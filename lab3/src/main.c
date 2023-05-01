ea44d0961a89978894a37403f1447eaef33bd842
Юрий <yr.olimpiev@gmail.com>
2023-05-01 21:45:51 +0700

    Починил вычисления, начал рефакторинг

--- lab3/src/main.c
+++ lab3/src/main.c
@@ -1,7 +1,16 @@
 /*
- * Created on Sat Apr 14 2023
- *
- * Copyright (c) 2023 Olimpiev Y. Y.
+ * Project: parallel matrix multiplication.
+ * Created Date: Friday, April 21st 2023, 3:20:34 pm
+ * Author: Olimpiev Y. Y.
+ * -----
+ * Last Modified:  yr.olimpiev@gmail.com
+ * Modified By: Olimpiev Y. Y.
+ * -----
+ * Copyright (c) 2023 NSU
+ * 
+ * All shall be well and all shall be well and all manner of things shall be well.
+ * Nope...we're doomed!
+ * -----
  */
 
 #include <assert.h>
@@ -83,77 +92,25 @@ int* InitTaskDispls(int* jobSizes, size_t gridParam) {
     return displs;
 }
 
-gsl_matrix* DistributeMatrixesOverTheGrid(gsl_matrix* matrixes[2], MPI_Comm comm, const int gridParam[2]) {
-    assert(matrixes);
-    assert(matrixes[0]);
-    assert(matrixes[1]);
-
-    gsl_matrix_transpose(matrixes[1]);
-    MPI_Comm subComm[2];
-    gsl_matrix* subMatrix[2] = {NULL, NULL};
-
-    for (size_t matrixIndex = 0; matrixIndex < 2; matrixIndex++) {
-        int rank = 0;
-        MPI_Comm_rank(comm, &rank);
-
-        int freeCoords[2] = {0, 1};
-
-        // Build sub communicator for rows.
-        if (MPI_Cart_sub(comm, freeCoords, &subComm[matrixIndex]) != MPI_SUCCESS) {
-            perror("Failed in cart sub\n");
-            return NULL;
-        }
-
-        int r = 0;
-        MPI_Comm_rank(subComm[matrixIndex], &r);
-
-        int* taskSizes = InitTaskSizes(matrixes[matrixIndex]->size1, gridParam[matrixIndex]);
-        int* taskDispls = InitTaskDispls(taskSizes, gridParam[matrixIndex]);
-
-        subMatrix[matrixIndex] = gsl_matrix_calloc(taskSizes[r], matrixes[matrixIndex]->size2);
-        for (int i = 0; i < gridParam[matrixIndex]; i++) {
-            taskSizes[i] *= matrixes[matrixIndex]->size2;
-            taskDispls[i] *= matrixes[matrixIndex]->size2;
-        }
-
-        MPI_Scatterv(matrixes[matrixIndex]->data + taskDispls[r], taskSizes, taskDispls, MPI_DOUBLE, subMatrix[matrixIndex]->data, taskSizes[r], MPI_DOUBLE, 0, subComm[matrixIndex]);
-
-        freeCoords[0] = 1;
-        freeCoords[1] = 0;
-    }
-
-    gsl_matrix* transSubB = gsl_matrix_calloc(subMatrix[1]->size2, subMatrix[1]->size1);
-    gsl_matrix_transpose_memcpy(transSubB, subMatrix[1]);
- 
-    gsl_matrix* subResult = gsl_matrix_calloc(subMatrix[0]->size1, transSubB->size2);
-    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, subMatrix[0], transSubB, 0.0, subResult);
-
+void GatherResultMatrix(gsl_matrix* subResult, const int gridParam[2], MPI_Comm comm, gsl_matrix* result) {
     int coords[2];
     int rank = 0;
     MPI_Comm_rank(comm, &rank);
 
-    gsl_matrix* result = NULL;
-
-
-    int size = 0;
-    MPI_Comm_size(comm, &size);
-
     if (rank == 0) {
-        result = gsl_matrix_calloc(matrixes[0]->size1, matrixes[1]->size2);
-        gsl_matrix_view resMatrixView = gsl_matrix_submatrix(result, 
-            0, 
-            0, 
-            subResult->size1,
-            subResult->size2);
+        int size = 0;
+        MPI_Comm_size(comm, &size);
+
+        gsl_matrix_view resMatrixView = gsl_matrix_submatrix(result, 0, 0, subResult->size1, subResult->size2);
         gsl_matrix_memcpy(&resMatrixView.matrix, subResult);
 
         for (int p = 1; p < size; p++) {
-            MPI_Cart_coords(comm, p, 2, coords);    
+            MPI_Cart_coords(comm, p, 2, coords);
 
             MPI_Recv(subResult->data, subResult->size1 * subResult->size2, MPI_DOUBLE, p, 1, comm, MPI_STATUS_IGNORE);
             resMatrixView = gsl_matrix_submatrix(result, 
-                result->size1 / gridParam[0] * coords[0], 
-                result->size2 / gridParam[1] * coords[1], 
+                result->size1 / gridParam[0] * coords[1], 
+                result->size2 / gridParam[1] * coords[0], 
                 subResult->size1,
                 subResult->size2);
             gsl_matrix_memcpy(&resMatrixView.matrix, subResult);
@@ -161,38 +118,85 @@ gsl_matrix* DistributeMatrixesOverTheGrid(gsl_matrix* matrixes[2], MPI_Comm comm
     } else {
         MPI_Send(subResult->data, subResult->size1 * subResult->size2, MPI_DOUBLE, 0, 1, comm);
     }
+}
 
+gsl_matrix* DistributeMatrixesOverTheGrid(gsl_matrix* matrixes[2], MPI_Comm comm, const int gridParam[2]) {
+    assert(matrixes);
+    // TODO: rebuild to transpose memcopy
+    gsl_matrix_transpose(matrixes[1]);
+
+    MPI_Comm subComm[2];
+    gsl_matrix* subMatrix[2] = {NULL, NULL};
+    // TODO: move to function-sender.
+        int freeCoords[2] = {0, 1};
+        for (size_t matrixIndex = 0; matrixIndex < 2; matrixIndex++) {
+            int rank = 0;
+            MPI_Comm_rank(comm, &rank);
+
+            if (MPI_Cart_sub(comm, freeCoords, &subComm[matrixIndex]) != MPI_SUCCESS) {
+                perror("Failed in cart sub\n");
+                return NULL;
+            }
+    
+            int* taskSizes = InitTaskSizes(matrixes[matrixIndex]->size1, gridParam[matrixIndex]);
+            int* taskDispls = InitTaskDispls(taskSizes, gridParam[matrixIndex]);
+
+            int r = 0;
+            MPI_Comm_rank(subComm[matrixIndex], &r);
+
+            subMatrix[matrixIndex] = gsl_matrix_calloc(taskSizes[r], matrixes[matrixIndex]->size2);
+            for (int i = 0; i < gridParam[matrixIndex]; i++) {
+                taskSizes[i] *= matrixes[matrixIndex]->size2;
+                taskDispls[i] *= matrixes[matrixIndex]->size2;
+            }
+
+            MPI_Scatterv(matrixes[matrixIndex]->data + taskDispls[r], taskSizes, taskDispls, MPI_DOUBLE, subMatrix[matrixIndex]->data, taskSizes[r], MPI_DOUBLE, 0, subComm[matrixIndex]);
+
+            freeCoords[0] = 1;
+            freeCoords[1] = 0;
+        }
+    // TODO: move to calc function
+    gsl_matrix* transSubB = gsl_matrix_calloc(subMatrix[1]->size2, subMatrix[1]->size1);
+    gsl_matrix_transpose_memcpy(transSubB, subMatrix[1]);
+
+    gsl_matrix* subResult = gsl_matrix_calloc(subMatrix[0]->size1, transSubB->size2);
+    gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, subMatrix[0], transSubB, 0.0, subResult);
+
+    // TODO: move to gather.
+    int coords[2];
+    int rank = 0;
+    MPI_Comm_rank(comm, &rank);
+
+    gsl_matrix* result = (rank == 0) ? gsl_matrix_calloc(matrixes[0]->size1, matrixes[1]->size2): NULL;
+    GatherResultMatrix(subResult, gridParam, comm, result);
     return result;
 }
 
-
 gsl_matrix* GridTopologyMatrixMultiplication(gsl_matrix* A, gsl_matrix* B, size_t gridWidth, size_t gridHeight) {
-    assert(A);
-    assert(B);
     assert(gridWidth > 0);
     assert(gridHeight > 0);
 
-    // Build topolody and communicators.
-    MPI_Comm grid_comm;
+    // Build topolody and communicator.
+    MPI_Comm gridComm;
     int dimSizes[2] = {gridWidth, gridHeight};
 
-    int wrap_around[2] = {0, 0};
-    if (MPI_SUCCESS != MPI_Cart_create(MPI_COMM_WORLD, 2, dimSizes, wrap_around, 0, &grid_comm)) {
+    int wrapAround[2] = {0, 0};
+    if (MPI_SUCCESS != MPI_Cart_create(MPI_COMM_WORLD, 2, dimSizes, wrapAround, 0, &gridComm)) {
         perror("Failed in cart create.\n");
         return NULL;
     }
 
     // matrixes distribution over the grid (A and B).
     gsl_matrix* matrixes[2] = {A, B};
-    return DistributeMatrixesOverTheGrid(matrixes, grid_comm, dimSizes);
+    return DistributeMatrixesOverTheGrid(matrixes, gridComm, dimSizes);
 }
 
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
-    gsl_matrix *matrixA = NULL;
-    gsl_matrix *matrixB = NULL;
-
+    gsl_matrix* matrixA = NULL;
+    gsl_matrix* matrixB = NULL;
+    // TODO: only root read matrixes.
     if (argc < 3) {
         perror("Wrong input. Expected params in command line args (gridWidth gridHeight)");
         return EXIT_FAILURE;
@@ -240,8 +244,20 @@ int main(int argc, char *argv[]) {
         perror("Wrong input. Use cli to pass params.\n");
         return EXIT_FAILURE;
     }
+
     int rank = 0;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (0 == rank) {
+        gsl_matrix* answer = gsl_matrix_calloc(matrixA->size1, matrixB->size2);
+        assert(answer);
+
+        gsl_blas_dgemm(CblasNoTrans, CblasNoTrans, 1.0, matrixA, matrixB, 0.0, answer);
+        fprintf(stdout, "Expected result:\n");
+        pretty_gsl_matrix_fprintf(stdout, answer, "%lf ");
+
+        gsl_matrix_free(answer);
+    }
+
     gsl_matrix *result = GridTopologyMatrixMultiplication(matrixA, matrixB, gridWidth, gridHeight);
     if (result && rank == 0) {
        pretty_gsl_matrix_fprintf(stdout, result, "%lf ");
